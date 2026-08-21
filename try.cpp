f50582ea9f2ce89c4df9eddc3e33aac1e88471c3
Stephenson-coder <johnstephenson1911@gmail.com>
2026-08-21 22:41:24 +0800

    asdf

---
+++ try.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    cout << "Barnak" << endl;
+    cout << "git config --global user.email johnstephenson1911@gmail.com" << endl;
+    cout << "git config --global user.name \"Stephenson-coder\"" << endl;
+
+    return  0;
+}
